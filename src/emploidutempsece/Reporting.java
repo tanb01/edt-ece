3ef09f8f4d592cf5f80c11cadbddf105d6e25bb2
tanb01 <tanb01@users.noreply.github.com>
2020-06-04 14:55:58 +0200

    Fix bug Reporting.java

--- src/emploidutempsece/Reporting.java
+++ src/emploidutempsece/Reporting.java
@@ -33,11 +33,13 @@ public class Reporting extends JFrame {
         super(applicationTitle);
         // On crée la dataset pour la SALLE
         //   PieDataset dataset = creationSallesReportingDataset(); // Pour diagramme
-        DefaultCategoryDataset dataset = creationSallesReportingHistoDataset(); // Pour histogramme
+                   PieDataset dataset = creationHeuresReportingDataset(); // Pour diagramme
+
+//        DefaultCategoryDataset dataset = creationSallesReportingHistoDataset(); // Pour histogramme
         // On crée la chart grâce à la dataset
         //  JFreeChart chart = creationDiag2D(dataset, chartTitle); // Pour un diagramme 2D
-        //  JFreeChart chart = creationDiag3D(dataset, chartTitle); // Pour un diagramme 3D
-        JFreeChart chart = creationHisto(dataset, chartTitle); // Pour un histogramme
+          JFreeChart chart = creationDiag3D(dataset, chartTitle); // Pour un diagramme 3D
+//        JFreeChart chart = creationHisto(dataset, chartTitle); // Pour un histogramme
         // On met la chart dans un panel
         ChartPanel chartPanel = new ChartPanel(chart);
         // Taille par défaut
@@ -48,8 +50,8 @@ public class Reporting extends JFrame {
 
     /**
      * Dataset du diagramme 2D et 3D pour les SALLES
-     * 
-     * @return 
+     *
+     * @return
      */
     private PieDataset creationSallesReportingDataset() {
         DefaultPieDataset result = new DefaultPieDataset();
@@ -63,45 +65,57 @@ public class Reporting extends JFrame {
         }
         return result;
     }
-    
+
     /**
      * Dataset de l'histogramme pour les SALLES
-     * 
-     * @return 
+     *
+     * @return
      */
     private DefaultCategoryDataset creationSallesReportingHistoDataset() {
         DefaultCategoryDataset dataset = new DefaultCategoryDataset();
         SiteDAO si = new SiteDAO();
         ArrayList<Site> sites = new ArrayList<Site>();
         sites = si.chercherTousLesSites();
-        
+
         for (Salle salle : sites.get(2).getSalles()) {
-            dataset.addValue(salle.getCapacite(), sites.get(2).getNomSite(), salle.getNomSalle()); 
+            dataset.addValue(salle.getCapacite(), sites.get(2).getNomSite(), salle.getNomSalle());
         }
         for (Salle salle : sites.get(3).getSalles()) {
-            dataset.addValue(salle.getCapacite(), sites.get(3).getNomSite(), salle.getNomSalle()); 
+            dataset.addValue(salle.getCapacite(), sites.get(3).getNomSite(), salle.getNomSalle());
         }
         return dataset;
     }
 
     /**
      * Dataset du diagramme 2D et 3D pour les SALLES
-     * 
-     * @return 
+     *
+     * @return
      */
     private PieDataset creationHeuresReportingDataset() {
         DefaultPieDataset result = new DefaultPieDataset();
 
         SeanceDAO se = new SeanceDAO();
         ArrayList<Seance> seances = new ArrayList<Seance>();
-        seances = se.chercherToutesLesSeances();
+        seances = se.chercherSeancesParGroupeId(31);
+        int nombresSeances = seances.size();
+        int nombresMinTotal = (90 * nombresSeances);
+        int nombresHeuresSeancesTotal = nombresMinTotal / 60;
+        nombresMinTotal %= 60;
+        
+//        ArrayList<Cours> cours = new ArrayList<Cours>();
+//        for (Seance seance : seances) {
+//            
+//            result.setValue(salle.getNomSalle(), salle.getCapacite());
+//            
+//        }
+//        result.setValue("Nombres d'heures du groupe 31", );
 
-        for (Seance seance : sites.get(2).getSalles()) {
-            result.setValue(salle.getNomSalle(), salle.getCapacite());
-        }
+
+        System.out.printf("%d:%02d", nombresHeuresSeancesTotal, nombresMinTotal);
+
         return result;
     }
-    
+
     /**
      * Création du diagramme 3D
      *
@@ -143,16 +157,16 @@ public class Reporting extends JFrame {
         );
         return pieChart;
     }
-    
+
     /**
      * Création de l'histogramme
-     * 
+     *
      * @param dataset
      * @param title
-     * @return 
+     * @return
      */
     private JFreeChart creationHisto(DefaultCategoryDataset dataset, String title) {
-        
+
         final JFreeChart barChart = ChartFactory.createBarChart(title, "Salles", "Capacité",
                 dataset, PlotOrientation.VERTICAL, true, true, false);
 
@@ -161,8 +175,8 @@ public class Reporting extends JFrame {
     }
 
     /**
-     * 
-     * @param args 
+     *
+     * @param args
      */
     public static void main(String[] args) {
         Reporting demo = new Reporting("Capacité des salles", "Salles :");
