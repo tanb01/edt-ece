d9bc3b197f17da830daba8f7548f02b8cf831a8b
N4na31 <ortiz_diana31@outlook.fr>
2020-06-07 18:33:44 +0200

    Update AdminVue.java

--- src/vue/AdminVue.java
+++ src/vue/AdminVue.java
@@ -13,7 +13,6 @@ import javax.swing.border.LineBorder;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
 import java.awt.event.*;
-import java.awt.print.PrinterException;
 import java.text.DateFormat;
 import java.text.MessageFormat;
 import java.text.SimpleDateFormat;
@@ -98,7 +97,7 @@ public class AdminVue extends JFrame {
     private ArrayList<JComboBox> comboModifierSeance = null;
     private JButton boutonValiderModificationSeance = null;
     private JButton boutonSelectionnerSeance = null;
-    private javax.swing.JButton print;
+   
     /**
      *
      * @param title
@@ -316,9 +315,7 @@ public class AdminVue extends JFrame {
         Font fG = new Font("Times New Roman", Font.BOLD, 12);
         UIManager.put("JButton.font", fG);
         
-        JButton print = new JButton("Imprimer");
-        print.setIcon(new ImageIcon("img/imprimer.png"));
-        center.add(print);
+       
         
 
         for (JButton bouton : boutons) {
@@ -485,15 +482,7 @@ public class AdminVue extends JFrame {
      * @param index
      */
 ////////////////////////////////////////////////////////////////////////////////    
-    private void printActionPerformed(java.awt.event.ActionEvent evt){ 
-        MessageFormat header = new MessageFormat("emploie du temps");
-        MessageFormat footer = new MessageFormat("page{0,number,integer}");
-        try{
-        tableEnGrille.print(JTable.PrintMode.NORMAL, header, footer);
-        }catch(java.awt.print.PrinterException e){
-            System.err.format("Erreur d'impression",e.getMessage());
-        }
-    }
+    
     
     public void selectSemaine(int index) {
         //boutons.get(index).setText("X");
